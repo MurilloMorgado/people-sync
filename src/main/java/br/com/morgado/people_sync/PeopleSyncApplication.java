c4e81f43b19c0ce13f660e80257109eb72786755
Murillo Morgado <mumorgadok@hotmail.com>
2024-09-16 21:20:44 -0300

    first commit

---
+++ src/main/java/br/com/morgado/people_sync/PeopleSyncApplication.java
@@ -0,0 +1,13 @@
+package br.com.morgado.people_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/morgado/people_sync/PeopleSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.morgado.people_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
